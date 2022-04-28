80a14c82d0fba5f8b1a2cecd5fb05e9af4fff8c6
rakibullahsazib <rakibullahsazib@yahoo.com>
2022-04-28 10:36:48 +0600

    initial commit with tailwind and api working

---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+import { defineNuxtConfig } from 'nuxt'
+
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+  typescript: {
+    shim: false
+  },
+  css: ["@/assets/css/styles.css"],
+  build: {
+    postcss: {
+      postcssOptions: require("./postcss.config.js"),
+    },
+  },
+})
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="text-xl">
+    Root
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
